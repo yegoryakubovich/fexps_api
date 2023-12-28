870049318567906cfb9c6b4b74a19b185dfd10fc
KEKACIK <arthur_0p@mail.ru>
2023-12-28 16:35:51 +0300

    Add new models to models/__init__.py

--- app/db/models/__init__.py
+++ app/db/models/__init__.py
@@ -28,11 +28,14 @@ from .text_translation import TextTranslation
 from .text_pack import TextPack
 from .method import Method
 from .requisite import Requisite
+from .contacts import Contact
 
 from .country import Country
 from .role_permission import RolePermission
+from .requisite import Requisite
 
 from .account import Account
 
 from .session import Session
 from .account_roles import AccountRole
+from .account_contacts import AccountContact
