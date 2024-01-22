c6c03b1561b5e07fc4e4d58f90cc2dcf095dd28c
yegoryakubovich <personal@yegoryakubovich.com>
2024-01-22 17:21:24 +0100

    License update

--- app/routers/client/languages/delete.py
+++ app/routers/client/languages/delete.py
@@ -1,5 +1,5 @@
 #
-# (c) 2023, Yegor Yakubovich, yegoryakubovich.com, personal@yegoryakybovich.com
+# (c) 2024, Yegor Yakubovich, yegoryakubovich.com, personal@yegoryakybovich.com
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
--- app/services/timezone.py
+++ app/services/timezone.py
@@ -1,5 +1,5 @@
 #
-# (c) 2023, Yegor Yakubovich, yegoryakubovich.com, personal@yegoryakybovich.com
+# (c) 2024, Yegor Yakubovich, yegoryakubovich.com, personal@yegoryakybovich.com
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,6 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+
 from app.db.models import Timezone
 from app.repositories.timezone import TimezoneRepository
 from app.services.base import BaseService
