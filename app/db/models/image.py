6e9194527e099d4c8ee142ca231dc9d3d6362a6f
KEKACIK <arthur_0p@mail.ru>
2024-02-27 18:19:25 +0300

    Add image and repository imports

---
+++ app/db/models/image.py
@@ -0,0 +1,30 @@
+#
+# (c) 2024, Yegor Yakubovich, yegoryakubovich.com, personal@yegoryakybovich.com
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
+from sqlalchemy import Column, BigInteger, Boolean, String
+
+from app.db.base_class import Base
+
+
+class Image(Base):
+    __tablename__ = 'images'
+
+    id = Column(BigInteger, primary_key=True)
+    id_str = Column(String(16))
+    model = Column(String(64))
+    model_id = Column(BigInteger)
+    is_deleted = Column(Boolean, default=False)
