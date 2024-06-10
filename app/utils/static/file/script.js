bdd8ae71a1d6d1291a4b7c66d383385ec4783cc8
KEKACIK <arthur_0p@mail.ru>
2024-06-10 19:19:47 +0300

    Add licence and minor fix

--- app/utils/static/file/script.js
+++ app/utils/static/file/script.js
@@ -1,5 +1,22 @@
+//
+// (c) 2024, Yegor Yakubovich, yegoryakubovich.com, personal@yegoryakybovich.com
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
 const fileInput = document.getElementById('file-input');
 const fileButton = document.getElementById('file-button');
+const continueButton = document.getElementById('continue-button')
 const imageContainer = document.getElementById('image-container');
 const fileMaxCount = 10;
 
@@ -10,7 +27,7 @@ fileButton.addEventListener('click', () => {
 fileInput.addEventListener('change', () => {
     const files = fileInput.files;
     imageContainer.innerHTML = '';
-    if (files.length > fileMaxCount){
+    if (files.length > fileMaxCount) {
         alert('max files / макс. файлов ' + fileMaxCount);
         return
     }
@@ -31,4 +48,8 @@ fileInput.addEventListener('change', () => {
         imgWrapper.appendChild(img);
         imageContainer.appendChild(imgWrapper);
     }
+});
+
+continueButton.addEventListener('click', () => {
+    window.history.back();
 });
