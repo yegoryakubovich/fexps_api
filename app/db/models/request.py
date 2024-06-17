4d174e79d851092e61a342e652f76c61cbcca703
KEKACIK <arthur_0p@mail.ru>
2024-06-17 10:10:35 +0300

    Many fix models,

--- app/db/models/request.py
+++ app/db/models/request.py
@@ -30,8 +30,7 @@ class RequestTypes:
 
 
 class RequestStates:
-    LOADING = 'loading'
-    WAITING = 'waiting'
+    CONFIRMATION = 'confirmation'
     INPUT_RESERVATION = 'input_reservation'
     INPUT = 'input'
     OUTPUT_RESERVATION = 'output_reservation'
@@ -39,20 +38,9 @@ class RequestStates:
     COMPLETED = 'completed'
     CANCELED = 'canceled'
 
-    choices = [WAITING, INPUT_RESERVATION, INPUT, OUTPUT_RESERVATION, OUTPUT, COMPLETED, CANCELED]
+    choices = [CONFIRMATION, INPUT_RESERVATION, INPUT, OUTPUT_RESERVATION, OUTPUT, COMPLETED, CANCELED]
     choices_rate_confirm = [INPUT_RESERVATION, INPUT]
     choices_finished = [COMPLETED, CANCELED]
-
-
-class RequestFirstLine:
-    INPUT_CURRENCY_VALUE = 'input_currency_value'
-    INPUT_VALUE = 'input_value'
-    OUTPUT_CURRENCY_VALUE = 'output_currency_value'
-    OUTPUT_VALUE = 'output_value'
-
-    choices = [INPUT_CURRENCY_VALUE, INPUT_VALUE, OUTPUT_CURRENCY_VALUE, OUTPUT_VALUE]
-    choices_input = [INPUT_CURRENCY_VALUE, INPUT_VALUE]
-    choices_output = [OUTPUT_CURRENCY_VALUE, OUTPUT_VALUE]
 
 
 class Request(Base):
@@ -62,32 +50,37 @@ class Request(Base):
     name = Column(String(length=32), nullable=True)
     wallet_id = Column(BigInteger, ForeignKey('wallets.id', ondelete='SET NULL'), nullable=True)
     wallet = relationship('Wallet', foreign_keys=wallet_id, uselist=False, lazy='selectin')
-    type = Column(String(length=8))
+    type = Column(String(length=16))
     state = Column(String(length=32))
-    rate_decimal = Column(Integer, default=2)
-    rate_confirmed = Column(Boolean, default=False)
-    difference_confirmed = Column(BigInteger, default=0)
-    first_line = Column(String(length=32))
-    first_line_value = Column(BigInteger)
-    input_currency_value_raw = Column(BigInteger, nullable=True)
-    input_currency_value = Column(BigInteger, nullable=True)
-    input_value_raw = Column(BigInteger, nullable=True)
-    input_value = Column(BigInteger, nullable=True)
-    input_rate_raw = Column(BigInteger, nullable=True)
-    input_rate = Column(BigInteger, nullable=True)
-    commission_value = Column(BigInteger, default=0)
-    rate = Column(BigInteger, nullable=True)
-    output_currency_value_raw = Column(BigInteger, nullable=True)
-    output_currency_value = Column(BigInteger, nullable=True)
-    output_value_raw = Column(BigInteger, nullable=True)
-    output_value = Column(BigInteger, nullable=True)
-    output_rate_raw = Column(BigInteger, nullable=True)
-    output_rate = Column(BigInteger, nullable=True)
+    rate_decimal = Column(Integer)
+    rate_fixed = Column(Boolean, default=False)
+    difference = Column(BigInteger)
+    difference_rate = Column(BigInteger)
+    commission = Column(BigInteger)
+    rate = Column(BigInteger)
+
     input_method_id = Column(BigInteger, ForeignKey('methods.id', ondelete='SET NULL'), nullable=True)
     input_method = relationship('Method', foreign_keys=input_method_id, uselist=False, lazy='selectin')
-    output_requisite_data_id = Column(BigInteger, ForeignKey('requisites_datas.id', ondelete='SET NULL'), nullable=True)
-    output_requisite_data = relationship('RequisiteData', foreign_keys=output_requisite_data_id,
-                                         uselist=False, lazy='selectin')
+    output_requisite_data_id = Column(
+        BigInteger,
+        ForeignKey('requisites_datas.id', ondelete='SET NULL'),
+        nullable=True,
+    )
+    output_requisite_data = relationship(
+        'RequisiteData',
+        foreign_keys=output_requisite_data_id,
+        uselist=False,
+        lazy='selectin',
+    )
     output_method_id = Column(BigInteger, ForeignKey('methods.id', ondelete='SET NULL'), nullable=True)
     output_method = relationship('Method', foreign_keys=output_method_id, uselist=False, lazy='selectin')
+
+    input_currency_value = Column(BigInteger, nullable=True)
+    input_rate = Column(BigInteger, nullable=True)
+    input_value = Column(BigInteger, nullable=True)
+
+    output_value = Column(BigInteger, nullable=True)
+    output_rate = Column(BigInteger, nullable=True)
+    output_currency_value = Column(BigInteger, nullable=True)
+
     is_deleted = Column(Boolean, default=False)
