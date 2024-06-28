c5bb672e7b600cc78af9aa85684a60a17b1098a9
KEKACIK <arthur_0p@mail.ru>
2024-06-28 17:25:50 +0300

    Minor fix

--- app/utils/exceptions/__init__.py
+++ app/utils/exceptions/__init__.py
@@ -27,7 +27,7 @@ from .method import MethodFieldsMissing, MethodFieldsParameterMissing, MethodPar
 from .notification import NotificationTelegramAlreadyLinked
 from .order import OrderRequestFieldsMissing, OrderRequestAlreadyExists, OrderStateWrong, OrderStateNotPermission, \
     OrderNotPermission, OrderRequestStateNotPermission, OrderRequestMaxValueError
-from .request import RequestStateWrong, RequestStateNotPermission, RequestRateNotFound
+from .request import RequestStateWrong, RequestStateNotPermission, RequestRateNotFound, RequestFoundOrders
 from .requisite import RequisiteMinimumValueError, RequisiteActiveOrdersExistsError, RequisiteStateWrong, \
     RequisiteNotEnough
 from .role import RoleAlreadyExist
