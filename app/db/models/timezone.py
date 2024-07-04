58aa6ea45fac181faedce2a8291b629a81946d76
KEKACIK <arthur_0p@mail.ru>
2024-07-04 17:32:08 +0300

    Remove from model "timezone" field "is_default"

--- app/db/models/timezone.py
+++ app/db/models/timezone.py
@@ -26,5 +26,4 @@ class Timezone(Base):
     id = Column(BigInteger, primary_key=True)
     id_str = Column(String(length=16))
     deviation = Column(Integer, default=0)
-    is_default = Column(Boolean, default=False)
     is_deleted = Column(Boolean, default=False)
